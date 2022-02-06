57e93728a0e3c3b194bcf3f1c97686c41587d521
gmaaddi <grm76@drexel.edu>
2022-02-06 14:37:47 -0500

    :rocket: Add Flower Variables
    Const for each flower icon, event listeners added to each icon, each have a console log message saying 'x button was clicked' where x is the specific flower icon

---
+++ main.js
@@ -0,0 +1,73 @@
+// buttons for each flower icon
+const daffodilBtn = document.getElementById('daffodil');
+const daisyBtn = document.getElementById('daisy');
+const lilyBtn = document.getElementById('lily');
+const roseBtn = document.getElementById('rose');
+const sunflowerBtn = document.getElementById('sunflower');
+const poppyBtn = document.getElementById('poppy');
+const geraniumBtn = document.getElementById('geranium');
+const marigoldBtn = document.getElementById('marigold');
+const chrysanthemumBtn = document.getElementById('chrysanthemum');
+const pansyBtn = document.getElementById('pansy');
+const carnationBtn = document.getElementById('carnation');
+const orchidBtn = document.getElementById('orchid');
+
+// Open/close daffodil (1)
+daffodilBtn.addEventListener('click', function() {
+    console.log('daffodil button clicked');
+});
+
+// Open/close daisy (2)
+daisyBtn.addEventListener('click', function() {
+    console.log('daisy button clicked');
+});
+
+// Open/close lily (3)
+lilyBtn.addEventListener('click', function() {
+    console.log('lily button clicked');
+});
+
+// Open/close rose (4)
+roseBtn.addEventListener('click', function() {
+    console.log('rose button clicked');
+});
+
+// Open/close sunflower (5)
+sunflowerBtn.addEventListener('click', function() {
+    console.log('sunflower button clicked');
+});
+
+// Open/close poppy (6)
+poppyBtn.addEventListener('click', function() {
+    console.log('poppy button clicked');
+});
+
+// Open/close geranium (7)
+geraniumBtn.addEventListener('click', function() {
+    console.log('geranium button clicked');
+});
+
+// Open/close marigold (8)
+marigoldBtn.addEventListener('click', function() {
+    console.log('marigold button clicked');
+});
+
+// Open/close chrysanthemum (9)
+chrysanthemumBtn.addEventListener('click', function() {
+    console.log('chrysanthemum button clicked');
+});
+
+// Open/close pansy (10)
+pansyBtn.addEventListener('click', function() {
+    console.log('pansy button clicked');
+});
+
+// Open/close carnation (11)
+carnationBtn.addEventListener('click', function() {
+    console.log('carnation button clicked');
+});
+
+// Open/close orchid (12)
+orchidBtn.addEventListener('click', function() {
+    console.log('orchid button clicked');
+});
